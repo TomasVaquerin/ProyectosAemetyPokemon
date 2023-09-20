fbbb72847d4bd0e3db4f3f5a68ea6693288ea02a
CuboAzul <83606915+CuboAzul@users.noreply.github.com>
2023-09-20 12:35:05 +0200

    syso

--- Aemet/src/main/java/org/example/Main.java
+++ Aemet/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Baby Hello");
+        System.out.println("Holaaaaaaaaaa");
     }
 }
